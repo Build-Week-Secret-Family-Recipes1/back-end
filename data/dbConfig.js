3a09e080afe1688a250a8c54151018b6d75d0533
Your name <a.m.verdine@gmail.com>
2020-05-28 11:32:27 -0400

    Update knex config

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,5 @@
 const knex = require("knex");
 const knexConfig = require("../knexfile");
-const nodeEnv = process.env.NODE_ENV || "development";
+const nodeEnv = process.env.NODE_ENV || "production";
 
 module.exports = knex(knexConfig[nodeEnv]);
