8e24580f000dc51f4420b61f498a845cc30f16d0
Your name <a.m.verdine@gmail.com>
2020-05-29 18:59:21 -0400

    Revert migration and seed file changes

--- data/migrations/20200523133106_initial.js
+++ data/migrations/20200523133106_initial.js
@@ -1,6 +1,6 @@
 exports.up = async function (knex) {
   await knex.schema.createTable("recipes", (table) => {
-    table.string("username", 128).notNullable();
+    table.integer("user_id").notNullable();
     table.increments("recipe_id");
     table.string("name").notNullable().unique();
     table.float("prep_time");
@@ -10,7 +10,7 @@ exports.up = async function (knex) {
   });
 
   await knex.schema.createTable("ingredients", (table) => {
-    table.string("username", 128).notNullable();
+    table.integer("user_id").notNullable();
     table.increments("ingr_id");
     table.text("ingr_name");
     table.float("amount");
@@ -18,7 +18,7 @@ exports.up = async function (knex) {
   });
 
   await knex.schema.createTable("instructions", (table) => {
-    table.string("username", 128).notNullable();
+    table.integer("user_id").notNullable();
     table.increments("instr_id");
     table.integer("step_#");
     table.text("instruction");
@@ -29,7 +29,7 @@ exports.up = async function (knex) {
     table.increments("inter_id");
     table.integer("recipe_id").notNullable();
     table.integer("ingr_id").notNullable();
-    table.string("username", 128).notNullable();
+    table.integer("user_id").notNullable();
   });
 
   await knex.schema.createTable("users", (table) => {
--- data/seeds/01_recipes.js
+++ data/seeds/01_recipes.js
@@ -7,7 +7,7 @@ exports.seed = function (knex) {
       source: "Grandma",
       img_path:
         "images/grilled-cheese-sandwich-87992575-57ebfdd13df78c690f385704.jpg",
-      username: "test_user",
+      user_id: 1,
     },
   ]);
 };
