244dd4cf36f905653284aa622088cebda658855c
Your name <a.m.verdine@gmail.com>
2020-05-28 20:06:43 -0400

    Update instructions route

--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -48,29 +48,10 @@ router.get("/:id/ingredients", async (req, res, next) => {
   }
 });
 
-router.get("/instructions", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const recipes = await db("instructions as i")
-      .join("recipes as r", "i.recipe_id", "r.recipe_id")
-      .join("ingredients as ig", "ig.ingr_id", "i.instr_id")
-
-      .select(
-        "i.recipe_id",
-        "r.name",
-        "r.prep_Time",
-        "r.category",
-        "ig.ingr_name",
-        "ig.amount",
-        "i.step_#",
-        "i.instruction",
-        "r.source"
-      );
-    res.json(recipes);
+    res.json(await db("instructions"));
   } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      errorMessage: "Cannot retrieve instructions",
-    });
     next(err);
   }
 });
