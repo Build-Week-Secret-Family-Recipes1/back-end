c9e8323e5c9ee6fcc030897c211fb36d7761342c
Your name <a.m.verdine@gmail.com>
2020-05-29 23:51:27 -0400

    Update migration and seed files

--- data/migrations/20200523133106_initial.js
+++ data/migrations/20200523133106_initial.js
@@ -1,6 +1,6 @@
 exports.up = async function (knex) {
   await knex.schema.createTable("recipes", (table) => {
-    table.integer("user_id").notNullable();
+    table.integer("user_id");
     table.increments("recipe_id");
     table.string("name").notNullable().unique();
     table.float("prep_time");
@@ -10,7 +10,7 @@ exports.up = async function (knex) {
   });
 
   await knex.schema.createTable("ingredients", (table) => {
-    table.integer("user_id").notNullable();
+    table.integer("user_id");
     table.increments("ingr_id");
     table.text("ingr_name");
     table.float("amount");
@@ -18,7 +18,7 @@ exports.up = async function (knex) {
   });
 
   await knex.schema.createTable("instructions", (table) => {
-    table.integer("user_id").notNullable();
+    table.integer("user_id");
     table.increments("instr_id");
     table.integer("step_#");
     table.text("instruction");
