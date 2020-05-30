0a82cd105f14f0f8dd9dc912b0c9f7238764a869
Your name <a.m.verdine@gmail.com>
2020-05-29 23:38:30 -0400

    Revert migration and seed file changes

--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -79,9 +79,9 @@ router.get("/:id/instructions", async (req, res, next) => {
 // *** ADD Recipe ***
 
 router.post("/", async (req, res) => {
-  const newRecipe = req.body;
+  const newRecipe = { ...req.body, user_id: req.params.id };
 
-  Users.addRecipe(newRecipe)
+  await Users.addRecipe(newRecipe)
     .then((recipe) => {
       res.status(201).json(recipe);
     })
