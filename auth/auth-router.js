0455b7c774da43bdaab7b2b0ffa9f8e4d48c2264
Your name <a.m.verdine@gmail.com>
2020-05-31 11:48:10 -0400

    Add session authentication for recipeRouter and userRouter

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -34,9 +34,9 @@ router.post("/register", (req, res) => {
 
 //implement user login
 router.post("/login", (req, res) => {
-  let { user_id, password } = req.body;
+  // let { user_id, password } = req.body;
 
-  Users.findBy({ user_id: req.body.user_id })
+  Users.findBy({ email: req.body.email })
     .first()
     .then((user) => {
       if (user && bcrypt.compareSync(password, user.password)) {
