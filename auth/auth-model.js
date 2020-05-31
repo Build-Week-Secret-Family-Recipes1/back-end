36ccdce26e3bdc280818dedba94f9b71b24aeafb
Your name <a.m.verdine@gmail.com>
2020-05-30 23:31:01 -0400

    Add session authentication for recipeRouter and userRouter

--- auth/auth-model.js
+++ auth/auth-model.js
@@ -16,21 +16,19 @@ function findBy(filter) {
   return db("users").where(filter);
 }
 
-function findById(id) {
-  return (
-    db("users")
-      .where({ id })
-      // .select("user_id", "email", "username", "password")
-      .first()
-  );
+function findById(user_id) {
+  return db("users")
+    .where({ user_id })
+    .select("user_id", "email", "username", "password")
+    .first();
 }
 
 async function add(user) {
-  const [id] = await db("users").insert(user);
+  const [user_id] = await db("users").insert(user, "user_id");
 
-  return findById(id);
+  return findById(user_id);
 }
 
-function remove(id) {
-  return db("users").where({ id }).del();
+function remove(user_id) {
+  return db("users").where({ user_id }).del();
 }
